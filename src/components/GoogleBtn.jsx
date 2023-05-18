b700bc65a329693eb43dbfea3cc1ae9415f22de7
Hasib24 <hasibislam1488@gmail.com>
2023-05-18 15:52:27 +0600

    setup google login system

--- src/components/GoogleBtn.jsx
+++ src/components/GoogleBtn.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContex } from '../providers/AuthContextProvider';
 
 const GoogleBtn = ({children}) => {
+    const {setUser, googleSignIn} = useContext(AuthContex)
+    const handleClick = () =>{
+        googleSignIn()
+        .then(result => setUser(result.user))
+        .catch(error => console.log(error.message))
+        
+    }
     return (
-        <button className='border'>{children}</button>
+        <button onClick={handleClick} className='border'>{children}</button>
     );
 };
 
--- src/providers/AuthContextProvider.jsx
+++ src/providers/AuthContextProvider.jsx
@@ -1,12 +1,27 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+import { getAuth } from "firebase/auth";
+import { app } from '../../firebase.config';
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 
 export const AuthContex = createContext(null)
 
 const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+
+
+    const googleSignIn = () =>{
+        return signInWithPopup(auth, googleProvider)
+    }
 
     const userInfo ={
-        user: "hasib"
+        user,
+        setUser,
+        googleSignIn
     }
 
     return (
