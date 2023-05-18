29ebff908b499ea8594ca006b87956611922eacb
Hasib24 <hasibislam1488@gmail.com>
2023-05-18 12:54:34 +0600

    setup nav link routing and active link style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,11 @@ import { NavLink } from 'react-router-dom';
 import { BsCarFront } from 'react-icons/bs';
 
 const Header = () => {
+    const navActvStyle = ({isActive}) =>{
+        return({
+            textDecoration: isActive ? 'underline' : 'none'
+        })
+    }
     return (
         <header className='flex items-center'>
             <div className='border flex justify-between container mx-auto'>
@@ -14,12 +19,12 @@ const Header = () => {
                     </div>
                 </div>
                 <nav className='border flex items-center'>
-                    <NavLink className='px-2 text-xl font-semibold'>Home</NavLink>
-                    <NavLink className='px-2 text-xl font-semibold'>All Toys</NavLink>
-                    <NavLink className='px-2 text-xl font-semibold'>My Toys</NavLink>
-                    <NavLink className='px-2 text-xl font-semibold'>Add A Toy</NavLink>
-                    <NavLink className='px-2 text-xl font-semibold'>Blog</NavLink>
-                    <NavLink className='px-2 text-xl font-semibold'>Login</NavLink>
+                    <NavLink style={navActvStyle} to='/' className='px-2 text-xl font-semibold'>Home</NavLink>
+                    <NavLink style={navActvStyle} to='/alltoys' className='px-2 text-xl font-semibold'>All Toys</NavLink>
+                    <NavLink style={navActvStyle} to='/mytoys' className='px-2 text-xl font-semibold'>My Toys</NavLink>
+                    <NavLink style={navActvStyle} to='/addatoy' className='px-2 text-xl font-semibold'>Add A Toy</NavLink>
+                    <NavLink style={navActvStyle} to='/blog' className='px-2 text-xl font-semibold'>Blog</NavLink>
+                    <NavLink style={navActvStyle} to='/login' className='px-2 text-xl font-semibold'>Login</NavLink>
                 </nav>
             </div>
         </header>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,8 +3,17 @@ import { createBrowserRouter } from 'react-router-dom';
 import HomeLayout from '../layouts/HomeLayout';
 import Home from '../pages/Home';
 import Error from '../pages/Error';
+import AllToys from '../pages/AllToys';
+import MyToys from '../pages/MyToys';
+import AddAToy from '../pages/AddAToy';
+import Blog from '../pages/Blog';
+import Login from '../pages/Login';
 
-
+// All Toys
+// My Toys
+// Add A Toy
+// Blog
+// Logi
 
 const router = createBrowserRouter([
     {
@@ -14,6 +23,26 @@ const router = createBrowserRouter([
         {
             path: "/",
             element: <Home></Home>
+        },
+        {
+          path: "/alltoys",
+          element: <AllToys></AllToys>
+        },
+        {
+          path: "/mytoys",
+          element: <MyToys></MyToys>
+        },
+        {
+          path: "/addatoy",
+          element: <AddAToy></AddAToy>
+        },
+        {
+          path: "/blog",
+          element: <Blog></Blog>
+        },
+        {
+          path: "/login",
+          element: <Login></Login>
         }
       ]
     },
