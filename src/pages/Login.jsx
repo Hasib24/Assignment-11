f02edda48b4a41e2c462ae66a53db2b09ffcd911
Hasib24 <hasibislam1488@gmail.com>
2023-05-18 13:05:25 +0600

    add login page skeleton

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    let [show, setShow] = useState(false);
+
+    const handleLogin = (e) =>{
+        e.preventDefault()
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log(email, password);
+    }
+
+        
+
     return (
-        <div>
-            login
+        <div className='border flex justify-center'>
+
+            <form className='py-5 border' onSubmit={handleLogin}>
+                <input className='m-2 p-1 border rounded-md outline-none' type="email" name="email" id="email" placeholder='Enter email' required /> <br />
+                <input className='m-2 p-1 border rounded-md outline-none' type={show ? 'password' : 'text'} name="password" id="password" placeholder='Password' required /> <br />
+                <p onClick={()=>{setShow(!show)}}>{show ? <span>show password</span> : <span>hide password</span> }</p>
+                <input className='m-2 p-1 border rounded-md cursor-pointer' type="submit" name="submit" id="submit" value="Login" />
+            </form>
+            
         </div>
     );
 };
