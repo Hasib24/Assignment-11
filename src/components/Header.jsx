5a36b26040fb53fc344c01dee6c0ea5eeed67046
Hasib24 <hasibislam1488@gmail.com>
2023-05-18 12:29:03 +0600

    add header and necessary pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { BsCarFront } from 'react-icons/bs';
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <header className='flex items-center'>
+            <div className='border flex justify-between container mx-auto'>
+                <div className='flex border items-center'>
+                    <BsCarFront className='text-6xl '></BsCarFront>
+                    <div className='p-3'>
+                        <h1 className='text-3xl font-bold'>ToyCar.com</h1>
+                        <span>A Toy Car Marketplace</span>
+                    </div>
+                </div>
+                <nav className='border flex items-center'>
+                    <NavLink className='px-2 text-xl font-semibold'>Home</NavLink>
+                    <NavLink className='px-2 text-xl font-semibold'>All Toys</NavLink>
+                    <NavLink className='px-2 text-xl font-semibold'>My Toys</NavLink>
+                    <NavLink className='px-2 text-xl font-semibold'>Add A Toy</NavLink>
+                    <NavLink className='px-2 text-xl font-semibold'>Blog</NavLink>
+                    <NavLink className='px-2 text-xl font-semibold'>Login</NavLink>
+                </nav>
+            </div>
+        </header>
     );
 };
 
