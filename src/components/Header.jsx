a44d90830fb2138e02789b0bb8f7a2adfd7ad703
Hasib24 <hasibislam1488@gmail.com>
2023-05-20 10:56:10 +0600

    make header mobile responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { BsCarFront } from 'react-icons/bs';
+import { MdClose } from 'react-icons/md';
+import { GoThreeBars } from 'react-icons/go';
 
 const Header = () => {
+    let [responsiveMenu, setResponsiveMenu ] = useState(false)
     const [myScroll, setMyScrollY] = useState(false)
     
     window.addEventListener("scroll", ()=>{
@@ -21,8 +24,8 @@ const Header = () => {
         })
     }
     return (
-        <header className={myScroll ? `z-10 flex items-center sticky top-0 bg-slate-50 duration-700 shadow-md`: `z-10 flex sticky top-0 items-center bg-none duration-700`}>
-            <div className=' flex justify-between container mx-auto'>
+        <header className={myScroll ? `z-10 px-5 flex items-center sticky top-0 bg-slate-50 duration-700 shadow-md`: `z-10 px-5 flex sticky top-0 items-center bg-none duration-700`}>
+            <div className=' flex justify-between items-center container mx-auto'>
                 <div className='flex items-center'>
                     <BsCarFront className='text-6xl text-red-800'></BsCarFront>
                     <div className='p-3'>
@@ -30,7 +33,10 @@ const Header = () => {
                         <span className='font-thin text-slate-600'>A Toy Car Marketplace</span>
                     </div>
                 </div>
-                <nav className=' flex items-center'>
+                <div className='text-3xl p-1 border  rounded-md md:hidden'>
+                    { responsiveMenu ? <MdClose onClick={()=>setResponsiveMenu(!responsiveMenu)}></MdClose> : <GoThreeBars onClick={()=>setResponsiveMenu(!responsiveMenu)}></GoThreeBars> }
+                </div>
+                <nav className={`text-xl flex flex-col md:block  p-2 ${responsiveMenu ? `absolute right-4 top-20 bg-[#363636de] rounded-md` : `absolute right-2 -top-48`} md:static md:bg-inherit duration-500`}>
                     <NavLink style={navActvStyle} className='px-3 text-xl text-red-700 tracking-tight font-semibold active:text-slate-500' to='/'>Home</NavLink>
                     <NavLink style={navActvStyle} className='px-3 text-xl text-red-700 tracking-tight font-semibold active:text-slate-500' to='/alltoys'>All Toys</NavLink>
                     <NavLink style={navActvStyle} className='px-3 text-xl text-red-700 tracking-tight font-semibold active:text-slate-500' to='/mytoys'>My Toys</NavLink>
