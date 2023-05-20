16ea08beb3369e4ea30b69dc1023fe3d78171d39
Hasib24 <hasibislam1488@gmail.com>
2023-05-20 23:25:57 +0600

    update login page design

--- src/components/GoogleBtn.jsx
+++ src/components/GoogleBtn.jsx
@@ -10,7 +10,7 @@ const GoogleBtn = ({children}) => {
         
     }
     return (
-        <button onClick={handleClick} className='border'>{children}</button>
+        <button onClick={handleClick} className=' border border-blue-600 duration-700 text-slate-800 active:bg-blue-500  cursor-pointer outline-none rounded-md my-3 w-full md:mx-auto py-2 font-semibold disabled:bg-slate-200'>{children}</button>
     );
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,19 +18,22 @@ const Login = () => {
         
 
     return (
-        <div className='border flex justify-center'>
+        <div className='p-8 my-10 max-w-md mx-auto justify-center border rounded-md shadow-lg'>
 
-            <form className='py-5 border' onSubmit={handleLogin}>
-                <input className='m-2 p-1 border rounded-md outline-none' type="email" name="email" id="email" placeholder='Enter email' required /> <br />
-                <input className='m-2 p-1 border rounded-md outline-none' type={show ? 'password' : 'text'} name="password" id="password" placeholder='Password' required /> <br />
+            <form className='p-8' onSubmit={handleLogin}>
+                <input className='outline-none border rounded-md my-3 w-full md:mx-auto' type="email" name="email" id="email" placeholder='Enter email' required /> <br />
+                <input className='outline-none border rounded-md my-3 w-full md:mx-auto' type={show ? 'password' : 'text'} name="password" id="password" placeholder='Password' required /> <br />
                 <p onClick={()=>{setShow(!show)}}>{show ? <span>show password</span> : <span>hide password</span> }</p>
-                <input className='m-2 p-1 border rounded-md cursor-pointer' type="submit" name="submit" id="submit" value="Login" />
+                <input className='bg-blue-600 border border-blue-600 duration-700 text-white active:bg-blue-500 hover:bg-white hover:text-slate-800 cursor-pointer outline-none rounded-md my-3 w-full md:mx-auto py-2 font-semibold disabled:bg-slate-200' type="submit" name="submit" id="submit" value="Login" />
               
-                <p>Do not have a account ? <Link to='/register'>Registeration</Link></p>
-                <GoogleBtn>login with <FcGoogle className='inline'></FcGoogle></GoogleBtn>
+                <p>Do not have a account ? <Link to='/register' className='text-blue-600 underline'>Registeration</Link></p>
 
 
             </form>
+            <div className='px-8'>
+                <h1 className=' text-center font-bold text-slate-700'>OR</h1>
+                <GoogleBtn><FcGoogle className='inline text-xl mx-2'></FcGoogle>Login with Google</GoogleBtn>
+            </div>
             
         </div>
     );
