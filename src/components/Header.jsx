e3c336af23641e8c6fadfd6c4d8c894b82501504
Hasib24 <hasibislam1488@gmail.com>
2023-05-20 10:11:30 +0600

    add header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { BsCarFront } from 'react-icons/bs';
 
 const Header = () => {
+    const [myScroll, setMyScrollY] = useState(false)
+    
+    window.addEventListener("scroll", ()=>{
+        setMyScrollY(true)
+        if(scrollY==0){
+            setMyScrollY(null)
+        }
+        
+    })
+    
+   
+
     const navActvStyle = ({isActive}) =>{
         return({
             textDecoration: isActive ? 'underline' : 'none'
         })
     }
     return (
-        <header className='flex items-center'>
-            <div className='border flex justify-between container mx-auto'>
-                <div className='flex border items-center'>
-                    <BsCarFront className='text-6xl '></BsCarFront>
+        <header className={myScroll ? `z-10 flex items-center sticky top-0 bg-slate-50 duration-700 shadow-md`: `z-10 flex sticky top-0 items-center bg-none duration-700`}>
+            <div className=' flex justify-between container mx-auto'>
+                <div className='flex items-center'>
+                    <BsCarFront className='text-6xl text-red-800'></BsCarFront>
                     <div className='p-3'>
-                        <h1 className='text-3xl font-bold'>ToyCar.com</h1>
-                        <span>A Toy Car Marketplace</span>
+                        <h1 className='text-3xl font-bold text-red-800'>ToyCar.com</h1>
+                        <span className='font-thin text-slate-600'>A Toy Car Marketplace</span>
                     </div>
                 </div>
-                <nav className='border flex items-center'>
-                    <NavLink style={navActvStyle} to='/' className='px-2 text-xl font-semibold'>Home</NavLink>
-                    <NavLink style={navActvStyle} to='/alltoys' className='px-2 text-xl font-semibold'>All Toys</NavLink>
-                    <NavLink style={navActvStyle} to='/mytoys' className='px-2 text-xl font-semibold'>My Toys</NavLink>
-                    <NavLink style={navActvStyle} to='/addatoy' className='px-2 text-xl font-semibold'>Add A Toy</NavLink>
-                    <NavLink style={navActvStyle} to='/blog' className='px-2 text-xl font-semibold'>Blog</NavLink>
-                    <NavLink style={navActvStyle} to='/login' className='px-2 text-xl font-semibold'>Login</NavLink>
+                <nav className=' flex items-center'>
+                    <NavLink style={navActvStyle} className='px-3 text-xl text-red-700 tracking-tight font-semibold active:text-slate-500' to='/'>Home</NavLink>
+                    <NavLink style={navActvStyle} className='px-3 text-xl text-red-700 tracking-tight font-semibold active:text-slate-500' to='/alltoys'>All Toys</NavLink>
+                    <NavLink style={navActvStyle} className='px-3 text-xl text-red-700 tracking-tight font-semibold active:text-slate-500' to='/mytoys'>My Toys</NavLink>
+                    <NavLink style={navActvStyle} className='px-3 text-xl text-red-700 tracking-tight font-semibold active:text-slate-500' to='/addatoy'>Add A Toy</NavLink>
+                    <NavLink style={navActvStyle} className='px-3 text-xl text-red-700 tracking-tight font-semibold active:text-slate-500' to='/blog'>Blog</NavLink>
+                    <NavLink style={navActvStyle} className='px-3 text-xl text-red-700 tracking-tight font-semibold active:text-slate-500' to='/login'>Login</NavLink>
                 </nav>
             </div>
         </header>
