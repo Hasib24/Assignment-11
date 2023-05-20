dcc5dda0a6513cc5b3699881d452dccbe2db307e
Hasib24 <hasibislam1488@gmail.com>
2023-05-20 23:55:00 +0600

    update regester page design

--- src/pages/Regestation.jsx
+++ src/pages/Regestation.jsx
@@ -17,20 +17,24 @@ const Regestation = () => {
    
 
     return (
-        <div className='border flex justify-center'>
+        <div className='p-8 my-10 max-w-md mx-auto justify-center border rounded-md shadow-lg'>
 
-            <form className='py-5 border' onSubmit={handleRegster}>
-                <input className='m-2 p-1 border rounded-md outline-none' type="name" name="name" id="name" placeholder='Name' /> <br />
-                <input className='m-2 p-1 border rounded-md outline-none' type="email" name="email" id="email" placeholder='Enter email' required /> <br />
-                <input className='m-2 p-1 border rounded-md outline-none' type={show ? 'password' : 'text'} name="password" id="password" placeholder='Password' required /> <br />
+            <form className='p-8' onSubmit={handleRegster}>
+                {/* something wrong with name field style  */}
+                <input className='outline-none border rounded-md my-3 w-full md:mx-auto' type="name" name="name" id="name" placeholder='Enter name' required /> <br />
+                <input className='outline-none border rounded-md my-3 w-full md:mx-auto' type="email" name="email" id="email" placeholder='Enter email' required /> <br />
+                <input className='outline-none border rounded-md my-3 w-full md:mx-auto' type={show ? 'password' : 'text'} name="password" id="password" placeholder='Password' required /> <br />
                 <p onClick={()=>{setShow(!show)}}>{show ? <span>show password</span> : <span>hide password</span> }</p>
-                <input className='m-2 p-1 border rounded-md outline-none' type="url" name="url" id="url" placeholder='url' />
-                <input className='m-2 p-1 border rounded-md cursor-pointer block' type="submit" name="submit" id="submit" value="Register" />
+                <input className='outline-none border rounded-md my-3 w-full md:mx-auto' type="url" name="url" id="url" placeholder='url' />
+                <input className='bg-blue-600 border border-blue-600 duration-700 text-white active:bg-blue-500 hover:bg-white hover:text-slate-800 cursor-pointer outline-none rounded-md my-3 w-full md:mx-auto py-2 font-semibold disabled:bg-slate-200' type="submit" name="submit" id="submit" value="Register" />
 
-                <p>Already have an accounte ? <Link to='/login'>Login</Link></p>
-                <GoogleBtn>login with <FcGoogle className='inline'></FcGoogle></GoogleBtn>
+                <p>Already have an accounte ? <Link to='/login' className='text-blue-600 underline'>Login</Link></p>
+                
 
             </form>
+            <div className='px-8'>
+                <GoogleBtn>login with <FcGoogle className='inline'></FcGoogle></GoogleBtn>
+            </div>
 
            
             
