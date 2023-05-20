14feae7b566f94a1078030eb0737f29fd939d417
Hasib24 <hasibislam1488@gmail.com>
2023-05-20 09:07:59 +0600

    add banner and error page design

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import bgVid from '../assets/bannar_vid.mp4';
+
 
 const Banner = () => {
     return (
-        <div>
-            banned
+        <div className='mb-5 h-[100vh]'>
+            <video src={bgVid} className='absolute top-0 h-[100vh] w-full object-cover -z-10' loop muted></video>
+            <div className='absolute top-0 h-[100vh] w-full bg-black opacity-20'>overlay</div>
+            <div className='absolute h-[100vh] w-full top-0 flex flex-col items-center justify-center border-red-500 '>
+                {/* <h1 className='text-5xl text-white font-bold'>Experience the Joyride</h1> */}
+                <h1 className='text-5xl text-white font-bold'>Find Your Perfect Toy Car</h1>
+                <h1 className='text-5xl text-white font-bold'>at ToyCar.com!</h1>
+
+            </div>
         </div>
     );
 };
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div>
-            error
-        </div>
+        <section className='container mx-auto'>
+            <div className='flex justify-center'><img className=' border w-auto object-cover' src="https://img.freepik.com/free-vector/internet-network-warning-404-error-page-file-found-web-page-internet-error-page-issue-found-network-404-error-present-by-man-sleep-display_1150-55450.jpg?w=740&t=st=1684527006~exp=1684527606~hmac=5232955252e97be88c3032838becd9426b17367f2d53f14eae7331e2f03d3a0d" alt="" /></div>
+            <div className='text-center '>
+                <h1 className='font-bold tracking-widest text-6xl'>404</h1>
+                <h1 className='tracking-wider text-2xl md:text-6xl'>Error has been occurred!</h1>
+            </div>
+        </section>
     );
 };
 
