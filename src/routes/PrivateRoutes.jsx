b8f41a8fcb8154a2aec8e880de98660ad1de4919
Hasib24 <hasibislam1488@gmail.com>
2023-05-21 22:30:35 +0600

    add react awsome spinner

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import { AuthContex } from '../providers/AuthContextProvider';
 import { Navigate } from 'react-router-dom';
+import { Ring } from 'react-awesome-spinners'
 
 const PrivateRoutes = ({ children }) => {
     const {user, loader} = useContext(AuthContex)
     if(loader){
-        return <div>loading...</div>
+        return <div className='h-[50vh] flex justify-center items-center'><Ring/></div>
     }
     if(user){
         return children
