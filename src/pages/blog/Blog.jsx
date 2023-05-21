de4603092a266c819e3dbdf449bbd4c27c73ccf3
Hasib24 <hasibislam1488@gmail.com>
2023-05-21 21:56:43 +0600

    update blog page answer

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -7,22 +7,22 @@ const qnAndAns = [
     {
         id: 1,
         qn: "What is an access token and refresh token? How do they work and where should we store them on the client-side?",
-        ans:""
+        ans:"In JSON Web Token (JWT) authentication, an access token and a refresh token are two separate components used for user authentication and authorization.  we store them on .env file of the client-side. "
     },
     {
         id: 2,
         qn: "Compare SQL and NoSQL databases?",
-        ans:""
+        ans:"SQL databases use SQL as the standardized query language and scale vertically by upgrading hardware resources. NoSQL databases provide flexible data models, such as key-value, document, columnar, or graph, which can handle unstructured or evolving data more effectively. They allow dynamic schema changes, making it easier to adapt to varying data structures. "
     },
     {
         id: 3,
         qn: "What is express js? What is Nest JS?",
-        ans: ""
+        ans: "Express.js is a fast and minimalist web application framework for Node.js, a runtime environment for executing JavaScript code on the server-side. Express.js simplifies the process of building web applications and APIs by providing a set of robust features and utilities. NestJS is a TypeScript-based web application framework built on top of Express.js. It follows a modular architecture, promotes decorators and dependency injection, and provides seamless integration with various libraries."
     },
     {
         id: 4,
         qn: "What is MongoDB aggregate and how does it work?",
-        ans:""
+        ans:"MongoDB's aggregate function is used to perform advanced data analysis and manipulation operations on collections. It allows you to apply a series of pipeline stages to transform and aggregate data, including filtering, grouping, sorting, projecting, and performing calculations. It provides powerful capabilities for data aggregation and processing within MongoDB."
     }
 ]
 
