c9b9619878a8b1eefd5d3138aaaa3c1456f7baf9
Hasib24 <hasibislam1488@gmail.com>
2023-05-21 15:50:14 +0600

    add toy card to react tab category

--- src/pages/home/TabOfToy.jsx
+++ src/pages/home/TabOfToy.jsx
@@ -1,32 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import Atoy from '../alltoys/Atoy';
+import TabToyCard from './TabToyCard';
 
 
 const TabOfToy = () => {
+    let [privateCar, setPrivateCar] = useState([])
+    let [hikingCar, setHikingCar] = useState([])
+    let [toyBusCar, setToyBusCar] = useState([])
+
+    useEffect(()=>{
+      fetch(`http://localhost:5000/alltoys/private-car`)
+      .then(res =>res.json())
+      .then(pCar => setPrivateCar(pCar))
+      
+      fetch(`http://localhost:5000/alltoys/hiking-car`)
+      .then(res =>res.json())
+      .then(data =>setHikingCar(data))
+
+      fetch(`http://localhost:5000/alltoys/toy-bus`)
+      .then(res =>res.json())
+      .then(data =>setToyBusCar(data))
+
+
+
+    },[])
+
+    // useEffect(()=>{
+    //   fetch(`http://localhost:5000/alltoys/hiking-car`)
+    //   .then(data =>setHikingCar(data))
+    // },[])
+
+  
+
+   
+
+    
     return (
         <section className='container mx-auto my-14'>
             <h1 className='text-4xl font-bold text-center'>Shop by Category</h1>
             <Tabs>
               <TabList>
-                <Tab>Mario</Tab>
-                <Tab>Luigi</Tab>
-                <Tab>Peach</Tab>
+                <Tab>Hiking Car</Tab>
+                <Tab>Privete Car</Tab>
+                <Tab>Toy Bus</Tab>
               </TabList>
 
               <TabPanel>
-                <p>
-                  game franchise, owned by Nintendo and created by Japanese video game designer
-                  Shige
-                </p>
+                <div className="grid grid-cols-3">
+                  {hikingCar.map(atoy =><TabToyCard atoy={atoy}></TabToyCard> )}
+                </div>
               </TabPanel>
 
               <TabPanel>
-                <p>Itali second</p>
+                <div className="grid grid-cols-3">
+                  {privateCar.map(atoy =><TabToyCard atoy={atoy}></TabToyCard> )}
+                </div>
               </TabPanel>
 
               <TabPanel>
-                <p>Peach ihe is the main playable character.</p>
+                <div className="grid grid-cols-3">
+                  {toyBusCar.map(atoy =><TabToyCard atoy={atoy}></TabToyCard> )}
+                </div>
               </TabPanel>
               
             </Tabs>
