0823d432ce0bbc597c712cffe7df4b920bd20ce4
Hasib24 <hasibislam1488@gmail.com>
2023-05-21 21:02:05 +0600

    restructure all toys page

--- src/pages/alltoys/AllToys.jsx
+++ src/pages/alltoys/AllToys.jsx
@@ -4,11 +4,37 @@ import Atoy from './Atoy';
 
 const AllToys = () => {
     const alltoys = useLoaderData()
-    console.log(alltoys);
+   
     return (
-        <section className='md:grid md:grid-cols-3 container mx-auto'>
-            {alltoys.map(atoy =><Atoy atoy={atoy} key={atoy._id}></Atoy>)}
-        </section>
+        <section>
+        <div className="container mx-auto my-10 relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Product name
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Price
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Category
+                        </th>
+                        
+                        <th scope="col" className="px-6 py-3 "  colSpan='2'>
+                            Update
+                        </th>
+                        <th scope='col'>Delete</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    {alltoys.map(toy=><Atoy toy={toy} key={toy._id}></Atoy>)} 
+                </tbody>
+            </table>
+        </div>
+
+    </section>
     );
 };
 
--- src/pages/alltoys/Atoy.jsx
+++ src/pages/alltoys/Atoy.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
+import { HiPencilAlt } from 'react-icons/hi';
+import { AiOutlineDelete } from 'react-icons/ai';
 
-const Atoy = ({atoy}) => {
+const Atoy = ({toy}) => {
 
 
-const { _id, name, url, price, category, sellerName, quantity} = atoy
+const { _id, name, url, price, category, sellerName, quantity} = toy
     console.log(name, url);
     return (
-        
-            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-                {/* //img */}
-                <a href="#">
-                    <img className="mb-3 h-60 w-full object-cover rounded-t-lg" src={url} alt="product image" />
-                </a>
-                <div className="px-5 pb-5">
-                    <div className='flex text-sm font-extralight justify-between text-slate-700'>
-                        <h3>Seller : {sellerName}</h3>
-                        <h3>Category : {category}</h3>
-                    </div>
-                    <h1 className="text-3xl font-semibold tracking-tight text-gray-900 dark:text-white">{name}</h1>
-                    <h3 className='font-extralight'>Avoilable : {quantity} pices</h3>
-                  
-                    <div className="flex items-center mt-2.5 mb-5">
-
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <span className="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
-                        <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">View details</a>
-                    </div>
-                </div>
-            </div>
+            <tr className="bg-white border-b hover:bg-gray-50 ">
+                {/* name  */}
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowra">
+                    {name}
+                </th>
+                {/* price  */}
+                <td className="px-6 py-4">
+                    ${price}
+                </td>
+                {/* category  */}
+                <td className="px-6 py-4">
+                    {category}
+                </td>
+                {/* action  */}
+                <td className="px-6 py-4" colSpan={2}>
+                    <button onClick={()=>handleUpdate(_id)} className='active:text-gray-400'>
+                        <HiPencilAlt className='inline'></HiPencilAlt>Update
+                    </button>
+                </td>
+                <td>
+                    <button onClick={()=>handleDelete(_id)} className='active:text-gray-400'>
+                        <AiOutlineDelete className='inline'></AiOutlineDelete> Delete
+                    </button>
+                </td>
+                
+            </tr>
 
     );
 };
