5d32a69ee95c48190a6bc699181007be809703e6
Hasib24 <hasibislam1488@gmail.com>
2023-05-21 20:45:10 +0600

    update slider photo link

--- src/pages/home/Slider.jsx
+++ src/pages/home/Slider.jsx
@@ -9,9 +9,9 @@ const Slider = () => {
     return (
         <section className='container mx-auto my-10'>
             <AutoplaySlider play={true} cancelOnInteraction={false}  interval={2000}>
-                <div data-src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" />
-                <div data-src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" />
-                <div data-src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" />
+                <div data-src="https://cdn.pixabay.com/photo/2021/02/13/05/32/model-car-6010382_960_720.jpg" />
+                <div data-src="https://cdn.pixabay.com/photo/2016/10/20/06/00/fiat-1754723_960_720.jpg" />
+                <div data-src="https://cdn.pixabay.com/photo/2020/04/24/11/45/model-car-5086548_960_720.jpg" />
             </AutoplaySlider>
         </section>
     );
