af1642dd8ebea0906f2048925d938a045caf98f9
Hasib24 <hasibislam1488@gmail.com>
2023-05-21 23:11:18 +0600

    final commit 3

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -5,7 +5,7 @@ import bgVid from '../../assets/bannar_vid.mp4';
 const Banner = () => {
     return (
         <div className='h-[100vh]'>
-            <video src={bgVid} className='absolute top-0 h-[100vh] w-full object-cover -z-10' loop muted></video>
+            <video src={bgVid} className='absolute top-0 h-[100vh] w-full object-cover -z-10' loop muted autoPlay></video>
             <div className='absolute top-0 h-[100vh] w-full bg-black opacity-20'>overlay</div>
             <div className='absolute h-[100vh] w-full top-0 flex flex-col items-center justify-center border-red-500 '>
                 {/* <h1 className='text-5xl text-white font-bold'>Experience the Joyride</h1> */}
