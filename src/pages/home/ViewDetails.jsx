37cbf6ceaaaad16c2d113c2b9122c7cecd11a279
Hasib24 <hasibislam1488@gmail.com>
2023-05-21 19:17:04 +0600

    update view details page

--- src/pages/home/ViewDetails.jsx
+++ src/pages/home/ViewDetails.jsx
@@ -5,11 +5,28 @@ const ViewDetails = () => {
 
     const toy = useLoaderData()
     console.log(toy);
+
+    const { _id, name, url, price, category, sellerName, email, quantity,rating, description} = toy[0]
     
     return (
-        <div>
-            view details
-        </div>
+        <section className='container mx-auto'>
+            <h1 className='text-5xl text-center font-bold'>Detailes</h1>
+            <div className='m-10 md:grid md:grid-cols-2 md:gap-3'>
+                <div><img className='rounded-lg' src={url} alt="Photo of a toy car" /></div>
+                <div>
+                    <h1>{category}</h1>
+                    <h1>Rating : {rating}</h1>
+                    <h1>Total : {quantity} pices</h1>
+                    <h1 className='text-2xl font-semibold'>Price : ${price}</h1>
+                    <h1 className='text-4xl font-bold my-5'>{name}</h1>
+                    <h1 className='my-5'>{description}</h1>
+                    <h1 className='text-slate-500'>Seller : {sellerName}</h1>
+                    <h1 className='text-slate-500'>Email : {email}</h1>
+                    
+
+                </div>
+            </div>
+        </section>
     );
 };
 
